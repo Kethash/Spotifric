0abf1fe40d17ae7fe9cf53df1827359a835137bf
Wuhan-Imouto <61096082+Wuhan-Imouto@users.noreply.github.com>
2020-10-20 15:42:19 +0200

    Components + routes
    Création , manque routes

---
+++ client/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <h2>Login</h2>
+            <form @submit.prevent="login"></form>
+            <input type="text" v-model="logs.username" placeholder="username" />
+            <input type="text" v-model="logs.password" placeholder="password" />
+        </div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {
+            logs: {
+                username: "",
+                password: "",
+            },
+        };
+    },
+
+    methods: {
+        login() {
+            this.$emit("login-user", this.logs);
+        },
+    },
+};
+</script>
+
+<style></style>
---
+++ client/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div>
+            <h2>Register</h2>
+            <form @submit.prevent="register"></form>
+            <input
+                type="text"
+                v-model="newUser.username"
+                placeholder="username"
+            />
+            <input
+                type="text"
+                v-model="newUser.password"
+                placeholder="password"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {
+            newUser: {
+                username: "",
+                password: "",
+            },
+        };
+    },
+
+    methods: {
+        register() {
+            this.$emit("register-user", this.newUser);
+        },
+    },
+};
+</script>
+
+<style></style>
