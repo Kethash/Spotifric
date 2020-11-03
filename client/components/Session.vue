54b5a95538f4a1cd262841471a93c971f5ddc22b
II-vico14-II <66569573+II-vico14-II@users.noreply.github.com>
2020-11-03 14:18:45 +0100

    front spotify login

--- client/components/Session.vue
+++ client/components/Session.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-      <aside class="menu_music">
+      <div class="sidebar">
+          <div class="sidebar-wrapper">
+
+              <div class="sidebar__logo">
+              
+              </div>
+
+              <div class="sidebar__menu">
+                  <a href="#"><i class="fas fa-home"> </i> Acceuil </a>
+                  <a href="#"><i class="fas fa-search"> </i> Rechercher </a>
+                  <a href="#"><i class="fas fa-database"> </i>  Bibliotheque </a>
+              </div>
+
+          </div>
+      </div>
+        
+
+
 
       </aside>
   </div>
@@ -21,5 +38,30 @@ module.exports = {
 </script>
 
 <style scoped>
+
+@import 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Roboto";
+}
+
+html, body{
+    height: 100%;
+
+}
+
+.sidebar{
+    float: left;
+    width: 250px;
+    padding: 10px 10px;
+    height: 100%;
+    overflow-y: auto;
+    background-color: black;
+    overflow-x: hidden;
+
+}
 
 </style>
