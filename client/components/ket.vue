5280ce8a4cbe8aa88f346070ebebd3ca04db1146
Wuhan-Imouto <61096082+Wuhan-Imouto@users.noreply.github.com>
2020-11-09 12:23:44 +0100

    music is now playable

--- client/components/ket.vue
+++ client/components/ket.vue
@@ -10,12 +10,16 @@
         </nav>
 
         <nav class="display_list">
-            <div @click="play(box.music)" class="musicbox" v-for="box in this.current_list" :key="box">
-                <div>
-                    {{ box.title }}
-                </div>
-                
+          <div
+            @click="play(box.music)"
+            class="musicbox"
+            v-for="box in this.current_list"
+            :key="box.title"
+          >
+            <div>
+              {{ box.title }}
             </div>
+          </div>
         </nav>
       </div>
       <div class="footer">
@@ -43,6 +47,23 @@
             </svg>
 
             <svg
+              v-if="playing"
+              width="3em"
+              height="3em"
+              viewBox="0 0 16 16"
+              class="bi bi-pause-fill"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              style="color: white"
+              @click.prevent="pause()"
+            >
+              <path
+                d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"
+              />
+            </svg>
+
+            <svg
+              v-if="!playing"
               width="3em"
               height="3em"
               viewBox="0 0 16 16"
@@ -50,6 +71,7 @@
               fill="currentColor"
               xmlns="http://www.w3.org/2000/svg"
               style="color: white"
+              @click.prevent="resume()"
             >
               <path
                 d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"
@@ -95,21 +117,39 @@ module.exports = {
         { titre: "Playlist 2", contenu: null },
       ],
 
-      current_list : [
-          {title: "Titre", music: "http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3"},
-          {title: "Titre", music: ""},
-          {title: "Titre", music: ""},
+      current_list: [
+        {
+          title: "Random",
+          music:
+            "http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3",
+        },
+        { title: "Random 2", music: "http://soundbible.com/grab.php?id=915&type=mp3" },
+        { title: "Rest", music: './components/mp3/Pheeno_Rest.mp3' },
+      ],
 
-    ],
-
+      nowPlaying: null,
+      playing: false,
     };
   },
 
   methods: {
-      play(audio) {
-          var single = new Audio(audio);
-          single.play()
-      }
+    play(audio) {
+      var single = new Audio(audio);
+      single.play();
+
+      this.nowPlaying = single;
+      this.playing = true;
+    },
+
+    pause() {
+      this.nowPlaying.pause();
+      this.playing = false;
+    },
+
+    resume() {
+      this.nowPlaying.play();
+      this.playing = true;
+    },
   },
 };
 </script>
@@ -124,24 +164,20 @@ module.exports = {
   position: fixed;
 }
 
-/* width */
 ::-webkit-scrollbar {
   width: 10px;
 }
 
-/* Track */
 ::-webkit-scrollbar-track {
-  background: rgb(49, 49, 49); 
-}
- 
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: rgb(112, 112, 112); 
+  background: rgb(49, 49, 49);
 }
 
-/* Handle on hover */
+::-webkit-scrollbar-thumb {
+  background: rgb(112, 112, 112);
+}
+
 ::-webkit-scrollbar-thumb:hover {
-  background: rgb(88, 88, 88); 
+  background: rgb(88, 88, 88);
 }
 
 .navigation {
@@ -193,22 +229,20 @@ module.exports = {
   overflow: auto;
 }
 
-
 .musicbox {
-    width: 200px;
-    height: 200px;
-    background-color: rgb(37, 37, 37);
-    font-size: 30px;
-    text-align: center;
-    margin-top: 25px;
-    margin-bottom: 25px;
-    border-radius: 20px;
+  width: 200px;
+  height: 200px;
+  background-color: rgb(37, 37, 37);
+  font-size: 30px;
+  text-align: center;
+  margin-top: 25px;
+  margin-bottom: 25px;
+  border-radius: 20px;
 }
 
 .musicbox:hover {
-    border: 1px solid lightgreen;
+  border: 1px solid lightgreen;
 }
-
 
 .footer {
   background-color: rgb(80, 80, 80);
@@ -224,14 +258,14 @@ module.exports = {
 .play_bar {
   width: 100%;
   height: 80%;
-  display:flex;
+  display: flex;
   justify-content: center;
 }
 
 .play_bar div {
-    width: 277px;
-    display: flex;
-    justify-content: space-around;
+  width: 277px;
+  display: flex;
+  justify-content: space-around;
 }
 
 .progress_bar {
