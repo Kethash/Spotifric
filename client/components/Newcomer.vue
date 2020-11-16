aba98e418d841a3fcdecdd5ba288ae271558c239
II-vico14-II <66569573+II-vico14-II@users.noreply.github.com>
2020-11-16 23:38:57 +0100

    images importés pour faire le "blog" comment spotifric marche avec effet parallax
    je fais sa demain, j'ai importe des images pour faire le front  en style parallax !

--- client/components/Newcomer.vue
+++ client/components/Newcomer.vue
@@ -1,21 +1,22 @@
 <template>
-  <article class="main">
-        <h1>Bienvenue dans un monde riche en musique</h1>
+    <div class="container">
+        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Praesentium, tempore! Odit, laborum. 
+            Ab asperiores ad quod corrupti necessitatibus libero natus fugit soluta,
+            incidunt deserunt ratione reprehenderit sequi ex accusamus quia.
+        </p>
+        <div class="parallax">
+        </div>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Perspiciatis officiis illo recusandae repudiandae tempora omnis nulla 
+            iure nobis autem repellat vitae amet beatae laudantium quia suscipit 
+            quidem, quasi ut at?
+        </p>
+    </div>
+    <p> JE COMPLETE DEMAIN LE STYLE PARALLAX AVEC LES PHTOS MISES </p>
 
-        <article class="bloc">
-            <h2>Ce site est l'alter-égo de Spotify mais par les riches pour les riches</h2>
-            <h1>Qu'es-ce que Spotify ?</h1>
-            <p>Spotify est un service de musique en streaming, il fonctionne sous abonnement mensuel avce différents tarifs selon le type d'abonnement</p>
-        </article>
-        <article class="bloc">
-
-        </article>
-        <article class="bloc">
-
-        </article>
-
-
-  </article>
+  
 </template>
 
 <script>
@@ -37,7 +38,34 @@ module.exports = {
 }
 </script>
 
+
 <style scoped>
+
+.container{
+    max-width: 960px;
+    margin: 0 auto;
+    background: #f9f9f9;
+    font-size: 24px;
+    padding: 25px;
+}
+
+.parallax{
+    background: url('components/bitcoin.jpg') no-repeat center;
+    background-size: cover;
+    background-attachment: fixed;
+    height: 500px;
+
+}
+
+
+
+
+</style>
+
+
+
+<!-- <style scoped>
+
 .main{
     background-color: white;
     width: 100%;
@@ -60,6 +88,30 @@ module.exports = {
 
 .bloc p {
     margin-left: 10px;
-}
+} 
 
-</style>
+
+
+</style> -->
+
+
+
+<!-- <article class="main">
+        <h1>Bienvenue dans un monde riche en musique</h1>
+
+        <article class="bloc">
+            <h2>Ce site est l'alter-égo de Spotify mais par les riches pour les riches</h2>
+            <h1>Qu'es-ce que Spotify ?</h1>
+            <p>Spotify est un service de musique en streaming, il fonctionne sous abonnement mensuel avce différents tarifs selon le type d'abonnement</p>
+        </article>
+        <article class="bloc">
+
+        </article>
+        <article class="bloc">
+
+        </article>
+
+
+  </article>
+
+  -->
