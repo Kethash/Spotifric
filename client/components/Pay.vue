b63c750c73fdff70d954cbde20ddcbf2fb98b78a
II-vico14-II <66569573+II-vico14-II@users.noreply.github.com>
2020-11-16 10:56:03 +0100

    k

--- client/components/Pay.vue
+++ client/components/Pay.vue
@@ -220,7 +220,7 @@ h4 {
   padding: 13px;
   background-color: #fcfcfc;
   display: inline-block;
-  float: left;
+  float: none;
   text-align: center;
   border: 1px solid #c0bfbf;
 }
@@ -246,7 +246,7 @@ h4 {
   width: 50%;
   padding: 12px;
   background-color: #fcfcfc;
-  float: left;
+  float: none;
   text-align: center;
   font-size: 16px;
   outline: none;
